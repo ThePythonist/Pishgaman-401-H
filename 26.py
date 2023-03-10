009928d9b854efc780d62567646f3cad07515585
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-03-10 09:49:53 +0330

    Add files via upload

---
+++ 26.py
@@ -0,0 +1,7 @@
+entry = ""
+
+while entry != "exit":
+    entry = input("Entry : ")
+    print(entry)
+else:
+    print("The flag has turned into exit")
---
+++ 27.py
@@ -0,0 +1,8 @@
+flag = 1
+
+while flag:
+    entry = input("Entry : ")
+    if entry == "exit":
+        flag = 0
+else:
+    print("The flag has turned into false")
