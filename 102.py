06332a375a9c64d1ca30b9915152df90b07a276b
Arash <53490252+ThePythonist@users.noreply.github.com>
2023-05-26 09:54:56 +0330

    Add files via upload

---
+++ 102.py
@@ -0,0 +1,7 @@
+from mymodule import *
+
+a = int(input("Enter any number : "))
+b = 4
+
+power(a, b)
+radical(a)
---
+++ 104.py
@@ -0,0 +1,13 @@
+import jdatetime
+
+# ---------------- Testing Jdatetime ----------------
+
+# test = str(jdatetime.datetime.now())
+# test2 = str(jdatetime.date.today())
+# date = test.split()
+# print(date)
+
+# ---------------- 2023 Christmas ----------------
+
+shamsi = jdatetime.date.fromgregorian(day=25, month=12, year=2023)
+print(shamsi)
